8ea14a487430c987a273417442397cbe4d0d0313
johnlg277-art <johnlg277@gmail.com>
2025-08-31 13:11:57 +0800

    Update settings.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -81,7 +81,6 @@ DATABASES = {
     )
 }
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
